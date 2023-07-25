409850bc07ee332c8f79b9e11307fa0e13253bc0
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-07-25 21:08:31 +0530

    theme switcher

---
+++ src/Components/Hello.js
@@ -0,0 +1,33 @@
+import React, {useContext} from "react"; 
+import ColorContext from "../Context/ColorContext";
+
+
+const Hello = ()=>{
+    let {defaultStyle, blackMode,blueMode, whiteMode} = useContext(ColorContext);
+
+    return(
+         <div>
+            <div style={{
+                    color: defaultStyle.color,
+                    backgroundColor: defaultStyle.backgroundColor,
+                    border: defaultStyle.border,
+                    textAlign: defaultStyle.textAlign,
+             }}>
+             <h1>Hey! How are you doing</h1>
+             <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam porro, ex dolor voluptate, odio aspernatur excepturi ea exercitationem quas aliquam asperiores adipisci eos ad modi vero deserunt nostrum quasi architecto explicabo. Necessitatibus dicta quaerat quos saepe quo, architecto, sapiente magnam error, ullam atque aperiam expedita consequuntur eveniet placeat laudantium ea?
+             </p>
+             
+        </div>
+             <button onClick={whiteMode}>White Mode</button> 
+             <button onClick={blackMode}>Dark Mode</button>
+             <button onClick={blueMode}>Blue Mode</button>
+
+
+         </div>
+    )
+
+}
+
+
+export default Hello;
---
+++ src/Context/ColorProvider.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import ColorContext from "./ColorContext";  
+
+
+let defaultStyle =  {color: "black",
+backgroundColor: "white",
+border: "1px solid  red",
+textAlign: "left"
+}
+
+const ColorProvider = (props) => {
+    let [style, setStyle] = useState(defaultStyle)
+
+
+ return(
+        <ColorContext.Provider value={
+            {
+            defaultStyle: style,
+            whiteMode: ()=>setStyle(defaultStyle),
+            blackMode: ()=>setStyle({...style, 
+                color: "white",
+                backgroundColor: "black",
+                border: "1px solid  white",
+                textAlign: "center",
+            }),
+            blueMode: ()=>setStyle({...style,
+                color: "green",
+                backgroundColor: "lightblue",
+                border: "1px solid  green",
+                textAlign: "right",
+            })
+            
+            }}>
+
+            {props.children}
+
+        </ColorContext.Provider>
+ )
+
+}
+
+export default ColorProvider;
+
+
+// 3 buttons = > 1 button => use one button to change all style 
+
+// localstroage:
+//
